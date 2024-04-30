1c9a1725eb07c7d213afa523b198d002234aa1f8
LafontRapnouilTristan <bananabegood212@gmail.com>
2024-04-30 09:53:23 +0200

    Hmsc setup

---
+++ R_scripts/paper_16s/Hmsc_pipeline/require_setup/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="hmsc",
+    version="0.1.0",
+    author="[removed for review]",
+    license='GPLv3+',
+    install_requires=[
+        'numpy==1.26.2',
+        'pandas==2.1.3',
+        'pyreadr==0.5.0',
+        'scipy==1.11.4',
+        'tensorflow==2.16.1',
+        'tensorflow-probability[tf]==0.24.0',
+        'ujson==5.8.0'
+    ]
+)
